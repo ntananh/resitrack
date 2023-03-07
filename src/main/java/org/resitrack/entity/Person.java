9e2a08fdd83868f109c525afe92718f29279439b
Giang-github <gn6724130@gmail.com>
2023-03-08 00:48:54 +0700

    Repairing core entities and entity class diagram

--- src/main/java/org/resitrack/entity/Person.java
+++ src/main/java/org/resitrack/entity/Person.java
@@ -1,59 +1,12 @@
 package org.resitrack.entity;
+
 import org.resitrack.enums.Gender;
-import java.util.Date;
+
+import java.time.LocalDate;
+
 public class Person {
     private String name;
     private Gender sex;
-    private Date dob;
+    private LocalDate dob;
     private long idNumber;
-    private String job;
-    private House house;
-
-    public House getHouse() {
-        return house;
-    }
-
-    public void setHouse(House house) {
-        this.house = house;
-    }
-
-    public Person() {
-    }
-
-    public Person(String name, Gender sex, House house) {
-        this.name = name;
-        this.sex = sex;
-        this.house = house;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Gender getSex() {
-        return sex;
-    }
-
-    public void setSex(Gender sex) {
-        this.sex = sex;
-    }
-
-    public Date getDob() {
-        return dob;
-    }
-
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
-
-    @Override
-    public String toString() {
-        return "Member : " + name + "\n" + "Gender : " + sex + "\n"
-                + "Birthday : " + dob + "\n" + "Citizen ID: " + idNumber + "\n"
-                + "Job : " + job + "\n"+"House Number: " + house.getNumberHouse() + "\n";
-    }
 }
