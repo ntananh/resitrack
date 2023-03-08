1ee03e18a4c105977e40a6963e8fcecbb2231b44
Giang-github <gn6724130@gmail.com>
2023-03-08 16:11:14 +0700

    Repairing attributes of each core entity

--- src/main/java/org/resitrack/entity/Person.java
+++ src/main/java/org/resitrack/entity/Person.java
@@ -5,6 +5,7 @@ import org.resitrack.enums.Gender;
 import java.time.LocalDate;
 
 public class Person {
+    private String id;
     private String name;
     private Gender sex;
     private LocalDate dob;
