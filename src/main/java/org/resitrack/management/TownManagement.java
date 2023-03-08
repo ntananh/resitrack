71dccf36037d19cc9312b4c506d0bed88e9ad940
thinh <sp.forever.t@gmail.com>
2023-03-08 18:35:18 +0700

    add TownManagement class

---
+++ src/main/java/org/resitrack/management/TownManagement.java
@@ -0,0 +1,14 @@
+package org.resitrack.management;
+
+import org.resitrack.entity.Town;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TownManagement {
+
+    public List<Town> findTownByName(String name) {
+        // add house to town logic
+        return new ArrayList<Town>();
+    }
+}
