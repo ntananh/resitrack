489f58b33c9c3e0aea599729cb719799d3077f99
thinh <sp.forever.t@gmail.com>
2023-03-14 14:21:39 +0700

    - in HouseController class:       + Add and edit java doc.       + Edit function createNewHouse().       + Delete getStreetName, validateStreet name - in House class:       + Delete getter Street Name,

--- src/main/java/org/resitrack/controller/HouseController.java
+++ src/main/java/org/resitrack/controller/HouseController.java
@@ -4,19 +4,30 @@ import org.resitrack.entity.House;
 import org.resitrack.entity.Town;
 import org.resitrack.util.CommonUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 /**
  * create a HouseController class with functions: add, delete, edit, search, display information about the house
+ *
  * @author TuDucThinh
  * @since 13/3/2023
  */
-
 public class HouseController {
-
+    /**
+     * This function makes the Scanner unchangeable
+     */
     private final Scanner scanner;
+    /**
+     * this function has access scope in the class.
+     * Data type is TownController variable name townController
+     */
     private TownController townController;
+    /**
+     * This function is used to create a list of houses.
+     * Helps users to store houses that have just been saved to town
+     */
     private List<House> houses;
 
     public HouseController() {
@@ -33,13 +44,14 @@ public class HouseController {
 
     /**
      * This function use for create a new house inside a town.
-     * After entering, you will be notified as follows: Successfully entered the house whose address is the information you just entered.
+     * After entering, you will be notified as follows: Successfully add the house
      * If you want to add another house, press 1 if you want to exit, press 0
      */
     public void createNewHouse() {
 
         String townId = getTownId();
-        int i = 1;
+        String nameTown = townController.getTownById(townId).getName();
+        int count = 1;
         int choice;
         if (townId == null) {
             System.out.println("Town is empty, could not create a new house. Please create a new town first");
@@ -47,29 +59,27 @@ public class HouseController {
         }
 
         do {
-            scanner.nextLine();
-            System.out.println("House " + i + ": ");
-            String streetName = getStreetName(townId);
-            String houseNumber = getHouseNumber(streetName, townId);
+            System.out.println("House " + count + ": ");
+            String houseNumber = getHouseNumber(townId);
 
-            this.houses.add(createHouse(houseNumber, streetName, townId));
-            System.out.println("\nAdded "+ i + " house. The latest home address is: House number "
-                    + houseNumber + ", " + streetName + " street");
+            this.houses.add(createHouse(houseNumber, townId));
+            System.out.println("\nAdded " + count + " house. The latest home address is: " + houseNumber + " " + nameTown);
             System.out.println();
             System.out.print("Enter 0 to exit, Enter 1 to continue: ");
             choice = scanner.nextInt();
-            if (choice == 1){
-                i++;
+            scanner.nextLine();
+            if (choice == 1) {
+                count++;
             }
-        }while (choice != 0);
+        } while (choice != 0);
 
 
     }
 
     /**
-     * this function is used to retrieve the value of ID town
-     * @return the neighborhood ID town
-     * @see String
+     * This function is used to retrieve the value of ID town
+     *
+     * @return the ID town
      */
     private String getTownId() {
 
@@ -85,7 +95,7 @@ public class HouseController {
         String townId = scanner.nextLine();
 
         while (!townController.isTownExisted(townId)) {
-            System.out.println("Town id:" + townId + "not found, please enter correct town id in list bellow: ");
+            System.out.println("Town id: " + townId + " not found, please enter correct town id in list bellow: ");
             townController.printAllTownInformation();
 
             System.out.print("Please enter town id again here: ");
@@ -96,65 +106,44 @@ public class HouseController {
     }
 
     /**
-     * this function is used to retrieve the value of house number
-     * @param streetName pass in the street name as String
+     * This function is used to retrieve the value of house number
+     *
      * @param townId pass ID town in String
      * @return the neighborhood number house
-     * @see String
      */
-    private String getHouseNumber(String streetName, String townId) {
+    private String getHouseNumber(String townId) {
 
         System.out.print("Enter house number: ");
         String houseNumber = scanner.nextLine();
-        String houseNumberError = validateHouseNumber(houseNumber, streetName, townId);
+        String houseNumberError = validateHouseNumber(houseNumber, townId);
 
         while (houseNumberError != null) {
             System.out.println(houseNumberError);
             System.out.print("Please enter house number again: ");
             houseNumber = scanner.nextLine();
-            houseNumberError = validateHouseNumber(houseNumber, streetName, townId);
+            houseNumberError = validateHouseNumber(houseNumber, townId);
         }
         return houseNumber;
     }
 
     /**
-     * this function is used to retrieve the value of street name
-     * @param townId pass ID town in String
-     * @return the neighborhood street name
-     * @see String
-     */
-    private String getStreetName(String townId) {
-
-        System.out.print("Enter street name: ");
-        String streetName = scanner.nextLine();
-        String streetNameError = validateStreetName(streetName, townId);
-
-        while (streetNameError != null) {
-            System.out.println(streetNameError);
-            System.out.print("Please enter street name again:");
-            streetName = scanner.nextLine();
-            streetNameError = validateStreetName(streetName, townId);
-        }
-        return streetName;
-    }
-
-    /**
-     * this function is used to retrieve the value of House Information
+     * This function is used to retrieve the value of House Information
+     *
      * @param house infuse the value of house in a House style
-     * @return house information like house ID, house number, street name, town name
-     * @see String
+     * @return house information like house ID, house number, town name
      */
     public String getHouseInfo(House house) {
 
         Town town = townController.getTownById(house.getTownId());
         return "House{" +
                 " id = '" + house.getId() + '\'' +
-                ", house number = '" + house.getHouseNumber() + "', street name = '" + house.getStreetName() + "', '" + town.getName() + '\'' +
+                ", house number = '" + house.getHouseNumber() + "', '" + town.getName() + '\'' +
                 '}';
     }
 
     /**
      * This function is used to print the information of the houses in the town to the screen
+     *
      * @return list of houses in town
      */
     public void printAllHouseInformation() {
@@ -166,15 +155,14 @@ public class HouseController {
 
     /**
      * This function is used to check the error of the house number entered by the user and give a message
+     *
      * @param houseNumber new house number
-     * @param streetName street name to check if given house number exist or not
-     * @param townId town id to check if given house number exist or not
+     * @param townId      town id to check if given house number exist or not
      * @return errors when user enters house number
-     * @see String
      */
-    private String validateHouseNumber(String houseNumber, String streetName, String townId) {
+    private String validateHouseNumber(String houseNumber, String townId) {
 
-        if (CommonUtil.isNullOrBlank(houseNumber) || CommonUtil.isNullOrBlank(streetName)) {
+        if (CommonUtil.isNullOrBlank(houseNumber)) {
             return "ERROR: House number cannot leave blank";
         }
 
@@ -183,45 +171,24 @@ public class HouseController {
                     "if your house number just have 1 character please add \"0\" before it";
         }
 
-        if (isHouseInTownExist(houseNumber, streetName, townId)) {
-            return "ERROR: House number " + houseNumber + " that exist in street '" + streetName + "' and town '" + townId + "'. Please enter another house number ";
+        if (isHouseInTownExist(houseNumber, townId)) {
+            return "ERROR: House number " + houseNumber + " that exist in town '" + townId + "'. Please enter another house number ";
         }
 
         return null;
     }
 
     /**
-     * This function is used to check the street name error entered by the user and give a message
-     * @param streetName new street name.
-     * @param townId town id to check if given street name exist or not
-     * @return errors when user enters street name
-     * @see String
-     */
-    private String validateStreetName(String streetName, String townId) {
-
-        if (CommonUtil.isNullOrBlank(streetName)) {
-            return "ERROR: Street name cannot leave blank";
-        }
-
-        if (streetName.trim().length() < 2) {
-            return "ERROR: Street name cannot less than 2 characters, ";
-        }
-
-        return null;
-    }
-
-    /**
-     * This function is used to check if the house number you just entered already exists in the town and street
+     * This function is used to check if the house number you just entered already exists in the town
+     *
      * @param houseNumber pass the house number as String
-     * @param streetName pass in the street name as String
-     * @param townId pass ID town in String
-     * @return true if the input value has the same house number, street name, tow ID already. if false return false
+     * @param townId      pass ID town in String
+     * @return true if the input value has the same house number, tow ID already. if false return false
      */
-    private boolean isHouseInTownExist(String houseNumber, String streetName, String townId) {
+    private boolean isHouseInTownExist(String houseNumber, String townId) {
 
         for (House house : houses) {
-            if (houseNumber.equalsIgnoreCase(house.getHouseNumber())
-                    && streetName.equalsIgnoreCase(house.getStreetName()) && townId.equalsIgnoreCase(house.getTownId())) {
+            if (houseNumber.equalsIgnoreCase(house.getHouseNumber()) && townId.equalsIgnoreCase(house.getTownId())) {
                 return true;
             }
         }
@@ -231,14 +198,14 @@ public class HouseController {
 
     /**
      * This function is used to transmit the necessary information of the house
+     *
      * @param houseNumber pass the house number as String
-     * @param streetName pass in the street name as String
-     * @param townId pass ID town in String
-     * @return Home with attributes like: house number, street name, townId
+     * @param townId      pass ID town in String
+     * @return Home with attributes like: house number, townId
      * @see House
      */
-    private House createHouse(String houseNumber, String streetName, String townId) {
+    private House createHouse(String houseNumber, String townId) {
 
-        return new House(houseNumber, streetName, townId);
+        return new House(houseNumber, townId);
     }
 }
